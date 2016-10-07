4f14d4f3895e7f7107dd8e061ad72ae5e2d0d996
pauldobyns <pauldobyns777@gmail.com>
2016-10-06 19:13:13 -0700

    Committing new HelloWorld created during session 2

---
+++ Session2-Paul/Session2-Paul/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class MyLittleProject
+{
+    static void Main()
+    {
+        Console.WriteLine("All your base are belong to us!!");
+
+        Console.ReadLine();
+    }
+}//END OF APPLICATION
